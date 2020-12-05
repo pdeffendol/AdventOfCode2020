6e8005b2d18b376980b114a02b07e521bdb8c27e
Pete Deffendol <pete@deffendol.org>
2020-12-05 09:36:10 -0700

    Add day 5 solution

---
+++ Day5/BoardingPass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class BoardingPass
+    {
+        public int Row { get; init; }
+        public int Column { get; init; }
+
+        public int GetSeatId()
+        {
+            return Row * 8 + Column;
+        }
+
+        public static BoardingPass FromString(string encodedPass)
+        {
+            int minRow = 0;
+            int maxRow = 127;
+            int minCol = 0;
+            int maxCol = 7;
+
+            foreach (char dir in encodedPass.Substring(0, 7))
+            {
+                if (dir == 'F')
+                {
+                    maxRow = (int)Math.Floor((minRow + maxRow) / 2.0);
+                }
+                else
+                {
+                    minRow = (int)Math.Ceiling((minRow + maxRow) / 2.0);
+                }
+            }
+
+            foreach (char dir in encodedPass.Substring(7, 3))
+            {
+                if (dir == 'L')
+                {
+                    maxCol = (int)Math.Floor((minCol + maxCol) / 2.0);
+                }
+                else
+                {
+                    minCol = (int)Math.Ceiling((minCol + maxCol) / 2.0);
+                }
+            }
+
+            return new BoardingPass { Column = minCol, Row = minRow };
+        }
+
+        public string ToString()
+        {
+            return $"Row {Row}, Column {Column}";
+        }
+
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var passes = File.ReadAllLines("input.txt").Select(BoardingPass.FromString);
+
+            var max = passes.Select(p => p.GetSeatId()).Max();
+            Console.WriteLine($"Max seat ID = {max}");
+
+            var allIds = passes
+                .Where(p => p.Row != 0 && p.Row != 127)
+                .Select(p => p.GetSeatId());
+
+            var mine = allIds
+                .First(id => !allIds.Contains(id + 1)
+                    && allIds.Contains(id + 2));
+
+            Console.WriteLine($"My seat ID = {mine}");
+        }
+    }
+}
+
