8c23613f01c1db043111383c5b3f1eaa735afcbc
Pete Deffendol <pete@deffendol.org>
2020-12-06 16:22:47 -0700

    Add Day 6 solution

---
+++ Day6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var groups = File.ReadAllText("input.txt").Split("\n\n");
+
+var anyYes = groups.Select(g => g.ToArray().Where(c => c >= 'a' && c <= 'z').Distinct().Count());
+
+Console.WriteLine($"Part 1: {anyYes.Sum()}");
+
+var questions = "abcdefghijklmnopqrstuvwxyz";
+var allYes = groups.Select(g =>
+{
+    var people = g.Trim().Split("\n");
+    return questions.Count(q => people.All(p => p.Contains(q)));
+});
+
+Console.WriteLine($"Part 2: {allYes.Sum()}");
