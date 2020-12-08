6fdfdedc4ce173e1c280be3591d1478826dc50e8
Pete Deffendol <pete@deffendol.org>
2020-12-08 08:28:09 -0700

    Add Day 7 solution

---
+++ Day7/Bag.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    public class Bag
+    {
+        public string Color { get; set; }
+        public Dictionary<string, int> Contained {get;set; } = new Dictionary<string, int>();
+
+        // plaid coral bags contain 3 posh fuchsia bags, 3 dotted beige bags, 2 wavy turquoise bags.
+        // bright gold bags contain no other bags.
+
+        private Regex containedRegex = new Regex(@"(\d+) ([\w\s]+) bags\.?");
+
+        public static Bag FromString(string bagString)
+        { 
+            var split = bagString.Split(" bags contain ");
+            var color = split[0];
+
+            var containedRegex = new Regex(@"((\d+) (\w+ \w+) bags?)");
+            var containedMatches = containedRegex.Matches(split[1]);
+
+            return new Bag
+            {
+                Color = color,
+                Contained = containedMatches.ToDictionary(m => m.Groups[3].Value, m => int.Parse(m.Groups[2].Value))
+            };
+        }
+    }
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bags = File.ReadAllLines("input.txt").Select(Bag.FromString).ToDictionary(k => k.Color);
+
+            var gold = FindAncestors(bags["shiny gold"], bags);
+
+            Console.WriteLine($"{gold.Count} bags can contain shiny gold");
+
+            var contained = BagsInside(bags["shiny gold"], bags);
+
+            Console.WriteLine($"{contained} bags are inside shiny gold");
+        }
+
+        private static Dictionary<string, Bag> FindAncestors(Bag current, Dictionary<string, Bag> allBags)
+        {
+            var direct = allBags.Where(b => b.Value.Contained.ContainsKey(current.Color)).ToDictionary(i => i.Key, i => i.Value);
+           
+            var all = new Dictionary<string, Bag>(direct);
+            foreach (var d in direct)
+            {
+                foreach (var i in FindAncestors(d.Value, allBags))
+                {
+                    if (!all.ContainsKey(i.Key))
+                        all.Add(i.Key, i.Value);
+                }
+            }
+
+            return all;
+        }
+
+        private static int BagsInside(Bag current, Dictionary<string, Bag> allBags)
+        {
+            var contained = 0;
+            foreach (var i in current.Contained)
+            { 
+                contained += i.Value;
+                contained += i.Value * BagsInside(allBags[i.Key], allBags);
+            }
+
+            return contained;
+        }
+    }
+}
